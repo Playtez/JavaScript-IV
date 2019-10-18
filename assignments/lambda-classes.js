add0b8e129886654e2c7dbd2c287a9d1c7fa1d9b
playtez <aaron.pleitez12@gmail.com>
2019-10-17 19:41:25 -0500

    stretch done

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -8,38 +8,12 @@
 //   - **Project Managers** - extensions of Instructors
 // - **IMPORTANT** - You'll need to create 2 - 3 objects for each class and test them according to their unique Attributes. For example:
 
-class Person {
-    constructor(attrs) {
-        this.name = attrs.name; // is this the right syntax
-        this.age = attrs.age;
-        this.location = attrs.location;
-    }
-    speak() {
-        return `Hello my name is ${this.name}, I am from ${this.location}`;
-    }
-} // end of person class
-
 // ^^^^^^^ Person ^^^^^^^^^
 
 // - First we need a Person class. This will be our `base-class`
 // - Person receives `name` `age` `location` all as props
 // - Person receives `speak` as a method.
 // - This method logs out a phrase `Hello my name is Fred, I am from Bedrock` where `name` and `location` are the object's own props
-
-class Instructor extends Person {
-    constructor(attrs) {
-        super(attrs);
-        this.specialty = attrs.specialty;
-        this.favLanguage = attrs.favLanguage;
-        this.catchPhrase = attrs.catchPhrase;
-    }
-    demo(subject) {
-        return `Today we are learning about ${subject}`;
-    }
-    grade(student, subject) {
-        return `${student.name} receives a perfect score on ${subject} `;
-    }
-}
 
 // ^^^^^^^ Instructor ^^^^^^
 
@@ -52,24 +26,6 @@ class Instructor extends Person {
 // - Instructor has the following methods:
 //   - `demo` receives a `subject` string as an argument and logs out the phrase 'Today we are learning about {subject}' where subject is the param passed in.
 //   - `grade` receives a `student` object and a `subject` string as arguments and logs out '{student.name} receives a perfect score on {subject}'
-
-class Student extends Instructor {
-    constructor(attrs) {
-        super(attrs);
-        this.previousBackground = attrs.previousBackground;
-        this.className = attrs.className;
-        this.favSubjects = attrs.favSubjects;
-    }
-    listSubjects() {
-        console.log(`${this.favSubjects} `);
-    }
-    PRAssignments(subject) {
-        return `${student.name} has submitted a PR for ${subject}`;
-    }
-    sprintChallenge(subject) {
-        console.log(`${student.name} has begun sprint challenge on ${subject}`);
-    }
-}
 
 // ^^^^^^ Student ^^^^^^^^
 
@@ -84,23 +40,6 @@ class Student extends Instructor {
 //   - `PRAssignment` a method that receives a subject as an argument and logs out that the `student.name has submitted a PR for {subject}`
 //   - `sprintChallenge` similar to PRAssignment but logs out `student.name has begun sprint challenge on {subject}`
 
-class ProjectManagers extends Student {
-    constructor(attrs) {
-        super(atttrs);
-        this.gradClassName = attrs.gradClassName;
-        this.favInstructor = attrs.favInstructor;
-    }
-
-    standup(slackChannel) {
-        console.log(
-            ` ${student.name} announces to ${slackChannel}, @channel standy times!​​​​​`
-        );
-    }
-    debugsCode(student, subject) {
-        console.log(`${name} debugs ${student.name}'s code on ${subject} `);
-    }
-}
-
 // #### Project Manager ^^^^^^^^
 
 // - Now that we have instructors and students, we'd be nowhere without our PM's
@@ -112,11 +51,183 @@ class ProjectManagers extends Student {
 //   - `standUp` a method that takes in a slack channel and logs `{name} announces to {channel}, @channel standy times!​​​​​
 //   - `debugsCode` a method that takes in a student object and a subject and logs out `{name} debugs {student.name}'s code on {subject}`
 
-const fred = new Instructor({
-    name: "Fred",
-    location: "Bedrock",
-    age: 37,
-    favLanguage: "JavaScript",
+class Person {
+    constructor(attrs) {
+        this.name = attrs.name; // is this the right syntax
+        this.age = attrs.age;
+        this.location = attrs.location;
+    }
+    speak() {
+        return `Hello my name is ${this.name}, I am from ${this.location}`;
+    }
+} // end of person class
+
+// ^^^^^^^ Person ^^^^^^^^^
+
+class Instructor extends Person {
+    constructor(attrs) {
+        super(attrs);
+        this.specialty = attrs.specialty;
+        this.favLanguage = attrs.favLanguage;
+        this.catchPhrase = attrs.catchPhrase;
+    }
+
+    demo(subject) {
+        return `Today we are learning about ${subject}`;
+    }
+    grade(student, subject) {
+        return `${student.name} receives a perfect score on ${subject} `;
+    }
+    testScore(student) {
+        return `You recieved a ${Math.random(student.graded)}`;
+    }
+}
+
+// ^^^^^^^ Instructor ^^^^^^
+
+class Student extends Person {
+    constructor(attrs) {
+        super(attrs);
+        this.previousBackground = attrs.previousBackground;
+        this.className = attrs.className;
+        this.favSubjects = attrs.favSubjects;
+        this.grade = attrs.grade;
+    }
+
+    listSubjects() {
+        return `${this.favSubjects} `;
+    }
+    PRAssignments(subject) {
+        return `${this.name} has submitted a PR for ${subject}`;
+    }
+    sprintChallenge(subject) {
+        return `${this.name} has begun sprint challenge on ${subject}`;
+    }
+}
+
+// ^^^^^^ Student ^^^^^^^^
+
+class ProjectManagers extends Instructor {
+    constructor(attrs) {
+        super(attrs);
+        this.gradClassName = attrs.gradClassName;
+        this.favInstructor = attrs.favInstructor;
+    }
+
+    standUp(slackChannel) {
+        return ` ${this.name} announces to ${slackChannel}, @channel standup times!​​​​​`;
+    }
+    debugsCode(student, subject) {
+        return `${this.name} debugs ${student.name}'s code on ${subject} `;
+    }
+}
+
+// #### Project Manager ^^^^^^^^
+
+// ======= Person ========
+const Aaron = new Person({
+    name: "Aaron",
+    location: "California",
+    age: 32
+});
+
+const Spencer = new Person({
+    name: "Spencer",
+    location: "USA",
+    age: 26
+});
+
+const Matt = new Person({
+    name: "Matt",
+    location: "somewhere",
+    age: 22
+});
+// ======= Instructor ========
+const Brit = new Instructor({
+    name: "Brit",
+    location: "Canada",
+    age: 32,
+    favLanguage: "Html",
     specialty: "Front-end",
     catchPhrase: `Don't forget the homies`
 });
+const Ryan = new Instructor({
+    name: "Ryan",
+    location: "USA",
+    age: 26,
+    favLanguage: "JavaScript",
+    specialty: "Front-end",
+    catchPhrase: `yo yo yo`
+});
+const Josh = new Instructor({
+    name: "Josh",
+    location: "somewhere",
+    age: 31,
+    favLanguage: "JavaScript and CSS",
+    specialty: "Front-end",
+    catchPhrase: `What up Gangsta`
+});
+
+// ======= Student ========
+const Billy = new Student({
+    name: "Billy",
+    location: "Canada",
+    age: 25,
+    previousBackground: "Barista",
+    className: "Web 80",
+    favSubjects: ["Anything with Coffee", "Life", "People", "Math"],
+    graded: 90
+});
+const Bobby = new Student({
+    name: "Bobby",
+    location: "USA",
+    age: 25,
+    previousBackground: "Astronaut",
+    className: "Web 40",
+    favSubjects: ["liberal Arts"],
+    graded: 90
+});
+const Nathan = new Student({
+    name: "Nathan",
+    location: "Unknown",
+    age: 31,
+    previousBackground: "Mechanic",
+    className: "Web 30",
+    favSubjects: ["Biology", "life"],
+    graded: 90
+});
+
+// ======= Project Manager========
+const Juan = new ProjectManagers({
+    name: "Juan",
+    location: "New Mexico",
+    age: 25,
+    gradClassName: "Web 2",
+    favInstructor: "Brit"
+});
+const Austin = new ProjectManagers({
+    name: "Austin",
+    location: "Idaho",
+    age: 28,
+    gradClassName: "Web 1",
+    favInstructor: "Josh"
+});
+const Dylan = new ProjectManagers({
+    name: "Dylan",
+    location: "Sioux Falls",
+    age: 31,
+    gradClassName: "Web 4",
+    favInstructor: "Brit"
+});
+
+// ======= ========
+
+console.log(Aaron.speak());
+console.log(Brit.demo("Html"));
+console.log(Ryan.grade(Aaron, "Math"));
+console.log(Billy.listSubjects());
+console.log(Bobby.PRAssignments("Math"));
+console.log(Nathan.sprintChallenge("Java"));
+console.log(Juan.standUp("WEB 25"));
+console.log(Austin.debugsCode(Billy, "JavaScript"));
+console.log(Austin.testScore(Bobby));
